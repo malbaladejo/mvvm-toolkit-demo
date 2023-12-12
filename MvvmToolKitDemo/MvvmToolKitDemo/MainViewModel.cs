2043c2eaf52cec88522be236817a0b81e248e779
malbaladejo <malbaladejo@free.fr>
2023-12-12 07:01:11 +0100

    MainViewModel temp

---
+++ MvvmToolKitDemo/MvvmToolKitDemo/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MvvmToolKitDemo
+{
+	internal class MainViewModel : ObservableObject
+	{
+		[ObservableProperty]
+		private int left = 0;
+	}
+}
